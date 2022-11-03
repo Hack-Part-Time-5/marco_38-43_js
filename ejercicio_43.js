b103b80e6d364aa93e819db08160fd2f80eaf8d5
Marco Antonio Galian <marcogalian@hotmail.com>
2022-11-03 21:20:52 +0100

    Arreglado ejercicio 43

--- ejercicio_43.js
+++ ejercicio_43.js
@@ -2,9 +2,17 @@
 Ejemplo [5, 5, 5, 5, 4, 5, 5] = 4, [2, 2, 2, 2, 3, 2, 2, 2, 2] = 3 */
 
 function unico (num) {
-    let resultado = num.filter(el => el !== num[0])
-    return resultado
-
+    num.sort((a , b) => a - b)
+    console.log(num) // [2, 2, 2, 2, 2, 2, 2, 2, 8]
+    if (num[0] === num[1]) { 
+        // Si el indice 0 del array num es igual al indice 1 de num, me devuelves el ultimo numero del array num.pop()
+        return num.pop()
+    } else {
+        // Y sino me devuelves el indice 0 de num
+        return num[0]
+    }
 }
-console.log(unico([2, 2, 2, 2, 7, 2, 2, 2, 2])) 
+
+console.log(unico([2, 2, 2, 2, 8, 2, 2, 2, 2])) 
+
 
